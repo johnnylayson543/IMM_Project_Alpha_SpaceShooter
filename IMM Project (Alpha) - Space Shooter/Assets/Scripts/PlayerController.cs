737a4ab879a7ed22693b6ddf07d13d74bbe6ed3d
SabreClaw86 <114063362+SabreClaw86@users.noreply.github.com>
2022-11-19 17:07:44 +0000

    update

--- IMM Project (Alpha) - Space Shooter/Assets/Scripts/PlayerController.cs
+++ IMM Project (Alpha) - Space Shooter/Assets/Scripts/PlayerController.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Private Variables
+    private float speed = 50.0f; // Set the Vector3/GameObject (Vehicle) variable n speed/frames/meters per 1 second
+    private float turnSpeed = 50.0f;
     private float horizontalInput;
-    private float speed = 10.0f;
+    private float forwardInput;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        // This is where we get player input
+        horizontalInput = Input.GetAxis("Horizontal"); // Set the float horizontalInput based on the GetAxis() method of the Input class (with the String literal "Horizontal" which is named, based and found on the Input Manager in the Project Settings where the game controllers are located)
+        forwardInput = Input.GetAxis("Vertical");
+
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);  
     }
 }
